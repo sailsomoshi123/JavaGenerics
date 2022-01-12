24485fad153ec64c63b2e03673f362cc135d2427
Sail Somoshi <sailsomoshi11@gmail.com>
2022-01-12 21:21:26 +0530

    REFACTORED:Generic class added

--- src/com/generics/GiveMax.java
+++ src/com/generics/GiveMax.java
@@ -1,30 +1,21 @@
 package com.generics;
 
-public class GiveMax {
+public class GiveMax <E>{
+    E element1;
+    E element2;
+    E element3;
+
+    public GiveMax(E element1, E element2, E element3) {
+        this.element1 = element1;
+        this.element2 = element2;
+        this.element3 = element3;
+    }
+
+
     public static void main(String[] args) {
-        int test1 = giveMax(3, 2, 1);
-        System.out.println("max number is: " + test1);
-        int test2 = giveMax(1, 3, 2);
-        System.out.println("max number is: " + test2);
-        int test3 = giveMax(1, 2, 3);
-        System.out.println("max number is: " + test3);
-        System.out.println("-------------------------");
-
-        float testFirst = giveMax(3.1F, 2.1F, 1.1F);
-        System.out.println("max number is: " + testFirst);
-        float testSecond = giveMax(1.1F, 3.1F, 2.1F);
-        System.out.println("max number is: " + testSecond);
-        float testThird = giveMax(1.1F, 2.1F, 3.1F);
-        System.out.println("max number is: " + testThird);
-        System.out.println("---------------------------");
-
-        String testWord1 = giveMax("yogesh", "soham", "sail");
-        System.out.println("Bigger string is: " + testWord1);
-        String testWord2 = giveMax("soham", "yogesh", "sail");
-        System.out.println("Bigger string is: " + testWord2);
-        String testWord3 = giveMax("sail", "soham", "yogesh");
-        System.out.println("Bigger string is: " + testWord3);
-        System.out.println("---------------------------");
+        System.out.println(GiveMax.giveMax(3,4,5));
+        System.out.println(GiveMax.giveMax("sail","yogesh","soham"));
+        System.out.println(GiveMax.giveMax(2.0F,3.1F,4.1F));
     }
 
     public static <E extends Comparable> E giveMax(E element1, E element2, E element3) {
